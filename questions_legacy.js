85469abc29fbc01a9591fdfa9412a754453a7f74
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-12-05 21:59:13 -0600

    Update questions_legacy.js

--- questions_legacy.js
+++ questions_legacy.js
@@ -1,201 +1,291 @@
 questions = [
     // Attract
     {
-        "id":1,
+        "id": 1,
         "question": "I would have relationships with men.",
         "effect": {
-            "attract": 1,
+            "attract": 1
         }
     },
     {
-        "id":2,
+        "id": 2,
         "question": "I could imagine myself in a sexual scenario with a man.",
         "effect": {
-            "attract": 1,
+            "attract": 1
         }
     },
     {
-        "id":3,
+        "id": 3,
         "question": "I find myself attracted to muscular bodies.",
         "effect": {
-            "attract": 1,
+            "attract": 1
         }
     },
     {
-        "id":4,
+        "id": 4,
         "question": "The benis only makes it better! (I like dick)",
         "effect": {
-            "attract": 1,
+            "attract": 1
         }
     },
     {
-        "id":5,
+        "id": 5,
         "question": "I find myself attracted to soft skin, feminine clothing.",
         "effect": {
-            "attract": -1,
+            "attract": -1
         }
     },
     {
-        "id":6,
+        "id": 6,
         "question": "I could imagine myself in a sexual scenario with a woman.",
         "effect": {
-            "attract": -1,
+            "attract": -1
         }
     },
     {
-        "id":7,
+        "id": 7,
         "question": "I would have relationships with women.",
         "effect": {
-            "attract": -1,
+            "attract": -1
+        }
+    },
+    {
+        "id": 8,
+        "question": "I enjoy admiring both men's and women's physiques equally.",
+        "effect": {
+            "attract": 0
+        }
+    },
+    {
+        "id": 9,
+        "question": "I find myself more drawn to traditionally masculine traits.",
+        "effect": {
+            "attract": 1
+        }
+    },
+    {
+        "id": 10,
+        "question": "I find myself more drawn to traditionally feminine traits.",
+        "effect": {
+            "attract": -1
         }
     },
 
     // Drive
     {
-        "id":8,
+        "id": 11,
         "question": "I sometimes find myself dreaming about lewd scenarios.",
         "effect": {
-            "drive": 1,
+            "drive": 1
         }
     },
     {
-        "id":9,
+        "id": 12,
         "question": "I would have sex on a regular basis.",
         "effect": {
-            "drive": 1,
+            "drive": 1
         }
     },
     {
-        "id":10,
+        "id": 13,
         "question": "I masturbate at least once a day.",
         "effect": {
-            "drive": 1,
+            "drive": 1
         }
     },
     {
-        "id":11,
+        "id": 14,
         "question": "I watch pornographic content.",
         "effect": {
-            "drive": 1,
+            "drive": 1
         }
     },
     {
-        "id":12,
+        "id": 15,
+        "question": "I feel neutral toward sexual activity.",
+        "effect": {
+            "drive": 0
+        }
+    },
+    {
+        "id": 16,
         "question": "I find myself repulsed by the idea of sex.",
         "effect": {
-            "drive": -2,
+            "drive": -2
         }
     },
     {
-        "id":13,
+        "id": 17,
         "question": "I could say 'chastity' is my virtue.",
         "effect": {
-            "drive": -2,
+            "drive": -2
+        }
+    },
+    {
+        "id": 18,
+        "question": "I feel a strong urge to pursue physical intimacy regularly.",
+        "effect": {
+            "drive": 1
+        }
+    },
+    {
+        "id": 19,
+        "question": "I feel content without any sexual activity in my life.",
+        "effect": {
+            "drive": -1
         }
     },
 
     // Dominant
     {
-        "id":14,
+        "id": 20,
         "question": "I like the idea of having power over someone.",
         "effect": {
-            "dominant": 1,
+            "dominant": 1
         }
     },
     {
-        "id":15,
+        "id": 21,
         "question": "I (would) enjoy someone having control over me.",
         "effect": {
-            "dominant": -1,
+            "dominant": -1
         }
     },
     {
-        "id":16,
+        "id": 22,
         "question": "I wouldn't be comfortable with getting dominated.",
         "effect": {
-            "dominant": 1,
+            "dominant": 1
+        }
+    },
+    {
+        "id": 23,
+        "question": "I enjoy relationships with a clear power dynamic.",
+        "effect": {
+            "dominant": 1
+        }
+    },
+    {
+        "id": 24,
+        "question": "I prefer relationships where power is shared equally.",
+        "effect": {
+            "dominant": 0
+        }
+    },
+    {
+        "id": 25,
+        "question": "I like exploring submissive tendencies in relationships.",
+        "effect": {
+            "dominant": -1
         }
     },
 
     // Deviance
     {
-        "id":17,
+        "id": 26,
         "question": "I have frowned-upon kinks/fetishes.",
         "effect": {
-            "deviance": 1,
+            "deviance": 1
         }
     },
     {
-        "id":18,
+        "id": 27,
         "question": "I belong to sub-communities associated with deviances.",
         "effect": {
-            "deviance": 1,
+            "deviance": 1
         }
     },
     {
-        "id":19,
+        "id": 28,
         "question": "I (would) enjoy degrading or getting degraded.",
         "effect": {
-            "deviance": 1,
+            "deviance": 1
         }
     },
     {
-        "id":20,
+        "id": 29,
         "question": "I (would) enjoy inflicting pain or having pain inflicted on me.",
         "effect": {
-            "deviance": 1,
+            "deviance": 1
         }
     },
     {
-        "id":21,
+        "id": 30,
         "question": "I consider myself vanilla.",
         "effect": {
-            "deviance": -2,
+            "deviance": -2
         }
     },
     {
-        "id":22,
-        "question": "I find most of fetishes disgusting.",
+        "id": 31,
+        "question": "I find most fetishes disgusting.",
         "effect": {
-            "deviance": -1,
+            "deviance": -1
         }
     },
     {
-        "id":23,
-        "question": "I might be too boring for some people in the bedroom.",
+        "id": 32,
+        "question": "I am curious about unconventional sexual preferences.",
         "effect": {
-            "deviance": -1,
+            "deviance": 1
+        }
+    },
+    {
+        "id": 33,
+        "question": "I avoid exploring anything outside the norm.",
+        "effect": {
+            "deviance": -1
+        }
+    },
+    {
+        "id": 34,
+        "question": "I feel most comfortable with light experimentation.",
+        "effect": {
+            "deviance": 0
         }
     },
 
     // Affect
     {
-        "id":24,
+        "id": 35,
         "question": "I could only be lewd to my partner(s).",
         "effect": {
-            "affect": 1,
+            "affect": 1
         }
     },
     {
-        "id":25,
+        "id": 36,
         "question": "Sex should be a romantic topic.",
         "effect": {
             "affect": 1
         }
     },
     {
-        "id":26,
+        "id": 37,
         "question": "I am okay with the idea of hook-ups or one-night stands.",
         "effect": {
             "affect": -1
         }
     },
     {
-        "id":27,
+        "id": 38,
         "question": "Sex is more about the pleasure than the intimacy.",
         "effect": {
             "affect": -1
         }
+    },
+    {
+        "id": 39,
+        "question": "I believe emotional connection is essential for physical intimacy.",
+        "effect": {
+            "affect": 1
+        }
+    },
+    {
+        "id": 40,
+        "question": "I think physical pleasure can exist independently of emotional connection.",
+        "effect": {
+            "affect": -1
+        }
     }
-
 ];
